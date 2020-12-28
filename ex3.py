1820ce0f4e5179b2b169ebfdf49a28b7256e3320
ijphel01 <phelps.isaac@gmail.com>
2020-12-27 21:51:53 -0500

    Add examples 1-5

---
+++ ex3.py
@@ -0,0 +1,35 @@
+# Prints the text
+print("I will now count my chickens:")
+
+# Print "Hens", divide 30 by 6 and add 25
+print("Hens", 25.0 + 30 / 6)
+# Print Roostes, 100 minus 3 times 25 modulus 4 (aka 3)
+print ("Roosters", 100.0 - 25 * 3 % 4)
+
+#Prints the text
+print("Now I will count the eggs:")
+
+#Returns 3 + 2 + 1 -5 + 0 - 1/4 + 6
+print (3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#Prints the text
+print("Is it true that 3 + 2 < 5 -7?")
+
+#Evaluates 5 less than -2
+print (3.0 + 2 < 5 - 7)
+
+#Prints text and 3+2
+print("What is 3 + 2?", 3.0 + 2)
+#Prints text and 5-7
+print ("What is 5 - 7?", 5.0 - 7)
+
+#Prints text
+print("Oh, that's why it's False.")
+
+#Prints text
+print("How about some more.")
+
+#Prints text and evaluations
+print("Is it greater?", 5.0 > -2)
+print("Is it greater or equal?", 5.0 >= -2)
+print("Is it less or equal?", 5.0 <= -2) 
